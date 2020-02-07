f097deb92913ff6fd9274387de51499a8447e4bd
Meha Agrawal <agra.meha@hotmail.com>
2020-02-07 00:48:31 -0500

    Fixed mobile view and nav bar coloring

--- index.js
+++ index.js
@@ -21,44 +21,44 @@ function updateTime() {
 updateTime();
 setInterval(updateTime, 1000);
 
-var nav = document.getElementsByTagName("nav")[0];
-var navLinks = nav.getElementsByTagName("a");
-var states = [];
-for (var i = 0; i < navLinks.length; i++) {
-  states.push(navLinks[i].getAttribute("href"));
-}
+// var nav = document.getElementsByTagName("nav")[0];
+// var navLinks = nav.getElementsByTagName("a");
+// var states = [];
+// for (var i = 0; i < navLinks.length; i++) {
+//   states.push(navLinks[i].getAttribute("href"));
+// }
 
-function highlightLink(linkIndex) {
-  for (let j = 0; j < navLinks.length; j++) {
-    if (j == linkIndex) {
-      navLinks[j].classList.add("active");
-    } else {
-      navLinks[j].classList.remove("active");
-    }
-  }
-}
+// function highlightLink(linkIndex) {
+//   for (let j = 0; j < navLinks.length; j++) {
+//     if (j == linkIndex) {
+//       navLinks[j].classList.add("active");
+//     } else {
+//       navLinks[j].classList.remove("active");
+//     }
+//   }
+// }
 
-for (let i = 0; i < navLinks.length; i++) {
-  navLinks[i].onclick = function() {
-    highlightLink(i);
-    uncheck();
-  };
-}
+// for (let i = 0; i < navLinks.length; i++) {
+//   navLinks[i].onclick = function() {
+//     highlightLink(i);
+//     uncheck();
+//   };
+// }
 
-function handleScroll() {
-  for (var i = navLinks.length - 1; i >= 0; i--) {
-    var relevantId = navLinks[i].getAttribute("href").slice(1);
-    if (document.getElementById(relevantId).getBoundingClientRect().y <= 20) {
-      highlightLink(i);
-      return;
-    }
-  }
-}
+// function handleScroll() {
+//   for (var i = navLinks.length - 1; i >= 0; i--) {
+//     var relevantId = navLinks[i].getAttribute("href").slice(1);
+//     if (document.getElementById(relevantId).getBoundingClientRect().y <= 20) {
+//       highlightLink(i);
+//       return;
+//     }
+//   }
+// }
 
-window.onload = handleScroll;
-document.getElementById("content").onscroll = handleScroll;
+// window.onload = handleScroll;
+// document.getElementById("content").onscroll = handleScroll;
 
-var checkbox = document.getElementById("hamburger-input");
-function uncheck() {
-  checkbox.checked = false;
-}
+// var checkbox = document.getElementById("hamburger-input");
+// function uncheck() {
+//   checkbox.checked = false;
+// }
